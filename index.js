ffaee48c165682be5b3041dae3c11b3265694f2d
Yesterday17 <t@yesterday17.cn>
2018-07-18 13:03:25 +0800

    完成后端

---
+++ index.js
@@ -0,0 +1,64 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+const fs = require("fs");
+
+const app = new Koa();
+const router = new Router();
+const config = "./tps.json";
+
+let store = {
+  now: {
+    tps: null,
+    time: "undefined"
+  },
+  lowest: {
+    tps: 21,
+    time: "undefined"
+  },
+  highest: {
+    tps: -1,
+    time: "undefined"
+  }
+};
+
+if (fs.existsSync(config)) {
+  store = JSON.parse(fs.readFileSync(config, { encoding: "utf-8" }));
+} else {
+  fs.writeFileSync(config, JSON.stringify(store, null, 2), {
+    encoding: "utf-8"
+  });
+}
+
+router.get("/", async (ctx, next) => {
+  ctx.body = JSON.stringify(store, null, 2);
+});
+
+router.post("/", async (ctx, next) => {
+  console.log(`Update TPS: ${ctx.request.query.tps}`);
+  let tps = parseFloat(ctx.request.query.tps);
+  if (tps !== NaN && tps <= 20 && tps >= 0) {
+    store.now.tps = tps;
+    store.now.time = new Date().getTime();
+
+    if (store.now.tps > store.highest.tps) {
+      with (store) {
+        [highest.tps, highest.time] = [now.tps, now.time];
+      }
+    }
+
+    if (store.now.tps < store.lowest.tps) {
+      with (store) {
+        [lowest.tps, lowest.time] = [now.tps, now.time];
+      }
+    }
+
+    ctx.body = JSON.stringify(store, null, 2);
+    fs.writeFileSync(config, JSON.stringify(store, null, 2), {
+      encoding: "utf-8"
+    });
+    return;
+  }
+  ctx.body = JSON.stringify({ message: "Illegal visit!" }, null, 2);
+});
+
+app.use(router.routes()).listen(8081);
